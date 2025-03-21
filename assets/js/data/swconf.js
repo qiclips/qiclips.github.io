b17ebe9b806f65315fc1ef5248d75bf68fb70b7c
worklt <worklt@163.com>
2025-03-21 18:38:53 +0800

    chore: change tagline

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742552849',resources: [
+    cacheName: 'chirpy-1742553520',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
