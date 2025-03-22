fa225a6891689fcc82b181373268dffe0b47143f
worklt <worklt@163.com>
2025-03-22 11:01:50 +0800

    docs: add lqip and remove image width and height set

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742553520',resources: [
+    cacheName: 'chirpy-1742612098',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
